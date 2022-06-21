4cbf16ceaac26638b2c17961e3a8b615d09eec6e
IgorSilva <boxigor13@gmail.com>
2022-06-21 20:38:35 -0300

    DDD

---
+++ beecrowd1050.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+
+namespace vamos
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+
+         
+        }
+        
+    }
+}
